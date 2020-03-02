16ee0a3327892f2b247dc940a4b5146989d8a501
Devon Hubner <devon@hubner.org>
2020-03-01 20:55:14 -0500

    S3 websites

--- production/20-s3-res.starwarsccg.org/res.tf
+++
@@ -1,9 +0,0 @@
-
-module "res_starwarsccg_org" {
-  source            = "../../modules/aws-s3-website"
-  s3_bucket_name    = "res.starwarsccg.org"
-  env_name          = "prod"
-  aws_region        = var.aws_region
-} ## module
-
-
