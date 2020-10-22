be829d457b43f26725bfe382930ec559d3644815
Devon Hubner <devon@hubner.org>
2020-10-21 22:46:24 -0400

    streaming.starwarsccg.org static website setup

---
+++ production/20-streaming.starwarsccg.org/output.tf
@@ -0,0 +1,47 @@
+
+
+output "aws_cloudfront_distribution_this_id" {
+  value = module.streaming_starwarsccg_org.aws_cloudfront_distribution_this_id
+}
+output "aws_cloudfront_distribution_this_arn" {
+  value = module.streaming_starwarsccg_org.aws_cloudfront_distribution_this_arn
+}
+output "aws_cloudfront_distribution_this_hosted_zone_id" {
+  value = module.streaming_starwarsccg_org.aws_cloudfront_distribution_this_hosted_zone_id
+}
+output "aws_cloudfront_distribution_this_domain_name" {
+  value = module.streaming_starwarsccg_org.aws_cloudfront_distribution_this_domain_name
+}
+
+output "aws_route53_record_this_name" {
+  value = module.streaming_starwarsccg_org.aws_route53_record_this_name
+}
+output "aws_route53_record_this_zone_id" {
+  value = module.streaming_starwarsccg_org.aws_route53_record_this_zone_id
+}
+
+output "aws_s3_bucket_this_id" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_id
+}
+output "aws_s3_bucket_this_arn" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_arn
+}
+output "aws_s3_bucket_this_bucket_domain_name" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_bucket_domain_name
+}
+output "aws_s3_bucket_this_bucket_regional_domain_name" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_bucket_regional_domain_name
+}
+output "aws_s3_bucket_this_hosted_zone_id" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_hosted_zone_id
+}
+output "aws_s3_bucket_this_region" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_region
+}
+output "aws_s3_bucket_this_website_endpoint" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_website_endpoint
+}
+output "aws_s3_bucket_this_website_domain" {
+  value = module.streaming_starwarsccg_org.aws_s3_bucket_this_website_domain
+}
+
