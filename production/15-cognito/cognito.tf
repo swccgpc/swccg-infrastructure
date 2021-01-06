cecfdcc7366027de0e91cd17f2a7e21597e60c38
Devon Hubner <devon@hubner.org>
2021-01-05 22:18:54 -0500

    added cognito output

--- production/15-cognito/cognito.tf
+++ production/15-cognito/cognito.tf
@@ -5,3 +5,5 @@
 module "starwarsccg" {
   source            = "../../modules/aws-cognito"
 } ## module
+
+
--- production/15-cognito/output.tf
+++ production/15-cognito/output.tf
@@ -1,1 +1,16 @@
 
+output "aws_cognito_user_pool_this_endpoint" {
+  description = "Cognito Pool Endpoint"
+  value       = module.starwarsccg.aws_cognito_user_pool_this_endpoint
+} ## output
+
+output "aws_cognito_user_pool_this_arn" {
+  description = "Cognito Pool ARN"
+  value       = module.starwarsccg.aws_cognito_user_pool_this_arn
+} ## output
+
+output "aws_cognito_user_pool_id" {
+  description = "Cognito Pool ID"
+  value       = module.starwarsccg.aws_cognito_user_pool_id
+} ## output
+
